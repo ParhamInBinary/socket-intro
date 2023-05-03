1615528162ed2295c38f3465b707058c18eddcb1
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-03 15:02:53 +0200

    further setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Socket, io } from "socket.io-client";
+import type {
+  ClientToServerEvents,
+  ServerToClientEvents,
+} from "../../server/communication.ts";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+export const socket: Socket<
+  ServerToClientEvents,
+  ClientToServerEvents
+> = io();
+
+ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
