d57d4f7f99a9ad6ab2b5e80a386b58b24d396526
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-03 15:49:07 +0200

    implemented socket and context provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Socket, io } from "socket.io-client";
-import type {
-  ClientToServerEvents,
-  ServerToClientEvents,
-} from "../../server/communication.ts";
 import App from "./App.tsx";
+import { SocketProvider } from "./context/socketContext.tsx";
 import "./index.css";
-
-
-export const socket: Socket<
-  ServerToClientEvents,
-  ClientToServerEvents
-> = io();
 
 ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 ).render(
   <React.StrictMode>
-    <App />
+    <SocketProvider>
+      <App />
+    </SocketProvider>
   </React.StrictMode>
 );
